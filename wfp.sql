deaa22f601c60012499342a496e9881f1d3d7098
xWampa <cristiantm96@gmail.com>
2021-06-21 01:54:09 +0200

    BD mySQL
    Con horas y fecha para las tareas

--- wfp.sql
+++ wfp.sql
@@ -1,81 +1,290 @@
-BEGIN;
-CREATE TABLE IF NOT EXISTS users (
-	id	INTEGER NOT NULL AUTO_INCREMENT,
-	login	TEXT NOT NULL,
-	name	TEXT NOT NULL,
-	email	TEXT,
-	passwd	TEXT NOT NULL,
-	PRIMARY KEY (id)
-);
-CREATE TABLE IF NOT EXISTS workflows (
-	id	INTEGER NOT NULL AUTO_INCREMENT,
-	name	TEXT NOT NULL,
-	PRIMARY KEY (id)
-);
-CREATE TABLE IF NOT EXISTS tasks (
-	id	INTEGER NOT NULL AUTO_INCREMENT,
-	name	TEXT NOT NULL,
-	workflow	INTEGER,
-	PRIMARY KEY(id),
-	FOREIGN KEY (workflow) REFERENCES workflows(id)
-);
-CREATE TABLE IF NOT EXISTS wftasks (
-	id	INTEGER NOT NULL AUTO_INCREMENT,
-	workflow	INTEGER NOT NULL,
-	orderaaa	INTEGER NOT NULL,
-	task	INTEGER NOT NULL,
-	data	TEXT,
-	PRIMARY KEY (id),
-	FOREIGN KEY (workflow) REFERENCES workflows(id),
-	FOREIGN KEY (task) REFERENCES tasks(id)
-);
-CREATE TABLE IF NOT EXISTS usertasks (
-	id	INTEGER NOT NULL AUTO_INCREMENT,
-	run	INTEGER NOT NULL,
-	user	INTEGER NOT NULL,
-	wftask	INTEGER NOT NULL,
-	state	INTEGER NOT NULL,
-	notes	TEXT,
-	PRIMARY KEY (id),
-	FOREIGN KEY (run) REFERENCES runs(id),
-	FOREIGN KEY (user) REFERENCES users(id),
-	FOREIGN KEY (wftask) REFERENCES wftasks(id)
-);
-CREATE TABLE IF NOT EXISTS runs (
-	id	INTEGER NOT NULL AUTO_INCREMENT,
-	workflow	INTEGER NOT NULL,
-	user	INTEGER NOT NULL,
-	state	INTEGER NOT NULL,
-	usertask	INTEGER,
-	PRIMARY KEY(id),
-	FOREIGN KEY (workflow) REFERENCES workflows(id),
-	FOREIGN KEY (usertask) REFERENCES usertasks(id),
-	FOREIGN KEY (user) REFERENCES users(id)
-);
-INSERT INTO users VALUES (1,'admin','Administrador','root@wf-processor.com','admin');
-INSERT INTO users VALUES (2,'juanito','Juan Sánchez','juan.sanchez@mycomp.com','12345');
-INSERT INTO users VALUES (3,'pepito','Pepe López','pepe.lopez@gmail.com','patata');
-INSERT INTO workflows VALUES (1,'Proceso1');
-INSERT INTO wftasks VALUES (1,1,10,1,'Al Scrum Master');
-INSERT INTO wftasks VALUES (2,1,20,4,'Reunión revisión del sprint');
-INSERT INTO wftasks VALUES (3,1,30,3,'Requisitos software para las tareas del sprint.');
-INSERT INTO tasks VALUES (1,'Enviar e-mail',NULL);
-INSERT INTO tasks VALUES (2,'Redatar documento',NULL);
-INSERT INTO tasks VALUES (3,'Revisar documento',NULL);
-INSERT INTO tasks VALUES (4,'Planificar reunión',NULL);
-INSERT INTO tasks VALUES (5,'Analizar funcionalidad',NULL);
-INSERT INTO tasks VALUES (6,'Diseñar funcionalidad',NULL);
-INSERT INTO tasks VALUES (7,'Implementar funcionalidad',NULL);
-INSERT INTO tasks VALUES (8,'Redactar requisitos de sistema',NULL);
-INSERT INTO tasks VALUES (9,'Diseñar pruebas de sistema',NULL);
-INSERT INTO tasks VALUES (10,'Realizar pedido',NULL);
-INSERT INTO tasks VALUES (11,'Enviar Factura',NULL);
-INSERT INTO runs VALUES (1,1,2,2,NULL);
-INSERT INTO usertasks VALUES (1,1,2,1,2,'');
-INSERT INTO usertasks VALUES (2,1,2,2,1,NULL);
-INSERT INTO usertasks VALUES (3,1,2,3,1,NULL);
-CREATE INDEX IF NOT EXISTS usertaskstate ON usertasks (
-	user,
-	state
-);
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 21, 2021 at 12:03 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.2.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wfp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `runs`
+--
+
+CREATE TABLE `runs` (
+  `id` int(11) NOT NULL,
+  `workflow` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
+  `state` int(11) NOT NULL,
+  `usertask` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `runs`
+--
+
+INSERT INTO `runs` (`id`, `workflow`, `user`, `state`, `usertask`) VALUES
+(1, 1, 2, 2, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `workflow` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `name`, `workflow`) VALUES
+(1, 'Enviar e-mail', NULL),
+(2, 'Redatar documento', NULL),
+(3, 'Revisar documento', NULL),
+(4, 'Planificar reunión', NULL),
+(5, 'Analizar funcionalidad', NULL),
+(6, 'Diseñar funcionalidad', NULL),
+(7, 'Implementar funcionalidad', NULL),
+(8, 'Redactar requisitos de sistema', NULL),
+(9, 'Diseñar pruebas de sistema', NULL),
+(10, 'Realizar pedido', NULL),
+(11, 'Enviar Factura', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` text NOT NULL,
+  `name` text NOT NULL,
+  `email` text DEFAULT NULL,
+  `passwd` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `name`, `email`, `passwd`) VALUES
+(1, 'admin', 'Administrador', 'root@wf-processor.com', 'admin'),
+(2, 'juanito', 'Juan Sánchez', 'juan.sanchez@mycomp.com', '12345'),
+(3, 'pepito', 'Pepe López', 'pepe.lopez@gmail.com', 'patata');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usertasks`
+--
+
+CREATE TABLE `usertasks` (
+  `id` int(11) NOT NULL,
+  `run` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
+  `wftask` int(11) NOT NULL,
+  `state` int(11) NOT NULL,
+  `notes` text DEFAULT NULL,
+  `horas` int(11) DEFAULT NULL,
+  `startdate` date DEFAULT NULL,
+  `enddate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `usertasks`
+--
+
+INSERT INTO `usertasks` (`id`, `run`, `user`, `wftask`, `state`, `notes`, `horas`, `startdate`, `enddate`) VALUES
+(1, 1, 2, 1, 2, '', NULL, NULL, NULL),
+(2, 1, 2, 2, 1, NULL, NULL, NULL, NULL),
+(3, 1, 2, 3, 1, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wftasks`
+--
+
+CREATE TABLE `wftasks` (
+  `id` int(11) NOT NULL,
+  `workflow` int(11) NOT NULL,
+  `orderaaa` int(11) NOT NULL,
+  `task` int(11) NOT NULL,
+  `data` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `wftasks`
+--
+
+INSERT INTO `wftasks` (`id`, `workflow`, `orderaaa`, `task`, `data`) VALUES
+(1, 1, 10, 1, 'Al Scrum Master'),
+(2, 1, 20, 4, 'Reunión revisión del sprint'),
+(3, 1, 30, 3, 'Requisitos software para las tareas del sprint.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `workflows`
+--
+
+CREATE TABLE `workflows` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `workflows`
+--
+
+INSERT INTO `workflows` (`id`, `name`) VALUES
+(1, 'Proceso1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `runs`
+--
+ALTER TABLE `runs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `workflow` (`workflow`),
+  ADD KEY `usertask` (`usertask`),
+  ADD KEY `user` (`user`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `workflow` (`workflow`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usertasks`
+--
+ALTER TABLE `usertasks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `run` (`run`),
+  ADD KEY `wftask` (`wftask`),
+  ADD KEY `usertaskstate` (`user`,`state`);
+
+--
+-- Indexes for table `wftasks`
+--
+ALTER TABLE `wftasks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `workflow` (`workflow`),
+  ADD KEY `task` (`task`);
+
+--
+-- Indexes for table `workflows`
+--
+ALTER TABLE `workflows`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `runs`
+--
+ALTER TABLE `runs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `usertasks`
+--
+ALTER TABLE `usertasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `wftasks`
+--
+ALTER TABLE `wftasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `workflows`
+--
+ALTER TABLE `workflows`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `runs`
+--
+ALTER TABLE `runs`
+  ADD CONSTRAINT `runs_ibfk_1` FOREIGN KEY (`workflow`) REFERENCES `workflows` (`id`),
+  ADD CONSTRAINT `runs_ibfk_2` FOREIGN KEY (`usertask`) REFERENCES `usertasks` (`id`),
+  ADD CONSTRAINT `runs_ibfk_3` FOREIGN KEY (`user`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`workflow`) REFERENCES `workflows` (`id`);
+
+--
+-- Constraints for table `usertasks`
+--
+ALTER TABLE `usertasks`
+  ADD CONSTRAINT `usertasks_ibfk_1` FOREIGN KEY (`run`) REFERENCES `runs` (`id`),
+  ADD CONSTRAINT `usertasks_ibfk_2` FOREIGN KEY (`user`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `usertasks_ibfk_3` FOREIGN KEY (`wftask`) REFERENCES `wftasks` (`id`);
+
+--
+-- Constraints for table `wftasks`
+--
+ALTER TABLE `wftasks`
+  ADD CONSTRAINT `wftasks_ibfk_1` FOREIGN KEY (`workflow`) REFERENCES `workflows` (`id`),
+  ADD CONSTRAINT `wftasks_ibfk_2` FOREIGN KEY (`task`) REFERENCES `tasks` (`id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
