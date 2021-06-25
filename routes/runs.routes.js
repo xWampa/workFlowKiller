6278aebd15211363f9044234f0554ea69e6cb085
Emilio Ramos <miliu16@hotmail.com>
2021-06-25 17:42:07 +0200

    completada ruta /runs que recibe id de workflow en el body y crea run y todas las usertasks correspondientes con sus estados

--- routes/runs.routes.js
+++ routes/runs.routes.js
@@ -3,10 +3,13 @@ const { check } = require('express-validator');
 
 const router = Router();
 
-const { runsGet } = require('../controllers/runs.controller');
+const { runsGet, runPost, wftaskGet } = require('../controllers/runs.controller');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 router.get('/', runsGet);
-
+router.post('/', [
+    check('workflow', 'Es necesario el workflow').not().isEmpty(),
+    validarCampos
+], runPost)
 
 module.exports = router;
